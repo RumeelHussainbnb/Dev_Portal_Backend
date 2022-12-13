405e4c7ed463662a0793fa587dbbd1fb6bf97c68
Osama Ishaq <osama.ishaq@ibex.co>
2022-12-13 23:42:57 +0500

    Pagination Api

--- server/controllers/martian.js
+++ server/controllers/martian.js
@@ -26,16 +26,32 @@ export default {
 
   onGetMartians: async (req, res) => {
     try {
+      const keyword = req.query.keyword;
+      const country = req.query.country;
+      const martian = req.query.martian;
+      const keywordRegex = new RegExp(keyword, 'i'); // i for case insensitive
+      const countryRegex = new RegExp(country, 'i');
+      const martianRegex = new RegExp(martian);
       const page = parseInt(req.query.pageNumber) || 10;
       const perPage = parseInt(req.query.limit) || 10;
       const skip = (page - 1) * perPage;
       const result = {};
-      const totalMartains = await Martian.countDocuments().exec();
+      let totalMartains = 0;
 
+      result.data = await Martian.find({
+        Expertise: { $regex: keywordRegex },
+        Country: { $regex: countryRegex },
+        MartianType: { $regex: martianRegex },
+      })
+        .skip(skip)
+        .limit(perPage)
+        .exec();
+      totalMartains = await Martian.countDocuments({
+        Expertise: { $regex: keywordRegex },
+        Country: { $regex: countryRegex },
+        MartianType: { $regex: martianRegex },
+      }).exec();
       result.totalMartains = totalMartains;
-
-      result.data = await Martian.find().skip(skip).limit(perPage).exec();
-
       res.status(201).json({ success: true, data: result });
     } catch (error) {
       res.status(400).json({ success: false, error: error });
